220888b6233326d2c50909be02c7fd7121016e70
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-01-02 20:35:12 +0100

    first commit

---
+++ src/GUI.py
@@ -0,0 +1,278 @@
+import tkinter as tk 
+from tkinter import ttk
+from tkinter import filedialog 
+from tkinter.scrolledtext import ScrolledText
+import game_mover
+import json
+
+config_json = "game_mover.json"
+debug = True
+
+class Config(object):
+    def __init__(self):
+        self.config_json = config_json
+        self.config = self.read_config_file()
+        self.launchers = dict()
+
+        for launcher in list(self.config["launchers"].keys()):
+            self.launchers[launcher] = game_mover.Launcher(launcher)
+        for launcher in self.launchers:
+            for folder in self.config["launchers"][launcher]["libraryFolders"]:
+                self.launchers[launcher].add_library_folder(folder)
+        self.last_selected = self.config["last_selected"]
+
+    def read_config_file(self):
+        try:
+            with open(self.config_json, "r") as file:
+                if debug: print("loaded json")
+                return json.load(file)
+        except FileNotFoundError:
+            with open(config_json, "w") as file:
+                d = dict()
+                d["last_selected"] = ""
+                d["launchers"] = dict()
+                json.dump(d, file)
+                if debug: print("created json")
+                return d
+
+    
+    def save(self, launcher_dict, last_selected):
+        self.config["last_selected"] = last_selected
+        self.config["launchers"] = dict()
+        for item in launcher_dict:
+            self.config["launchers"][item] = dict()
+            self.config["launchers"][item]["libraryFolders"] = []
+            for folder in launcher_dict[item].libraryFolders:
+                self.config["launchers"][item]["libraryFolders"].append(folder.path)
+        with open(self.config_json, "w") as file:
+            json.dump(self.config, file)
+    
+    def write_config_file(self, dic):
+        with open(self.config_json, "w") as file:
+            json.dump(self.config, file)
+
+class MainFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.config = Config()
+        self.launchers = self.config.launchers
+        self.launcher_names = [launcher for launcher in self.launchers]
+
+        self.selected_launcher = tk.StringVar()
+        self.selected_launcher.set(self.config.last_selected)
+
+        self.launcher_frame = LauncherFrame(self)
+        self.launcher_frame.grid(column=0, row=0, sticky=("N", "W"), padx=5, pady=10)
+
+        self.libview_frame = LibViewFrame(self)
+        self.libview_frame.grid(column=0, row=1, sticky=("N", "W", "E", "S"), pady=10)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight = 0)
+        self.rowconfigure(1, weight = 1)
+
+
+    def save_config(self):
+        self.config.save(self.launchers, self.selected_launcher.get())
+
+        pass
+        
+
+    
+
+
+class LauncherFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        
+        #self.selected_launcher = tk.StringVar()
+
+        self.selected_launcher_combobox = ttk.Combobox(self, textvariable=self.master.selected_launcher)
+        self.selected_launcher_combobox.state(["readonly"])
+        self.selected_launcher_combobox.bind("<<ComboboxSelected>>", self.on_launcher_change)
+        self.selected_launcher_combobox.grid(column=0, row=0, sticky=("N", "W", "S"))
+
+        self.add_launcher_button = ttk.Button(self, text="Add Launcher", command=self.on_add_launcher)
+        self.add_launcher_button.grid(column=1, row=0, sticky=("N", "W", "S"))
+
+        self.add_lib_button = ttk.Button(self, text="Add Folder", command=self.on_add_lib)
+        self.add_lib_button.grid(column=2, row=0, sticky=("N", "W", "S", "E"))
+
+        self.refresh()
+
+    def refresh(self):
+        names = [launcher for launcher in self.master.launchers]
+        names.sort()
+        if debug: print(names)
+        self.selected_launcher_combobox["values"] = names
+        self.selected_launcher_combobox.set(self.master.selected_launcher.get())
+
+    def on_add_launcher(self, event=None):
+        launcher = LauncherDialog(self).show()
+        self.master.launchers[launcher] = game_mover.Launcher(launcher)
+        self.master.selected_launcher.set(launcher)
+        self.master.save_config()
+        self.refresh()
+
+    def on_add_lib(self, event=None):
+        #folder = FolderDialog(self).show()
+        folder = filedialog.askdirectory()
+        self.master.launchers[self.master.selected_launcher.get()].add_library_folder(folder)
+        self.master.libview_frame.refresh()
+        self.master.save_config()
+
+    def on_launcher_change(self, event=None):
+        if debug: print("switched to " + self.master.selected_launcher.get())
+        self.master.libview_frame.refresh()
+        self.master.save_config()
+
+class LibViewFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.labels = []
+        self.trees = []
+        self.scrollbars = []
+        self.buttons = []
+        self.del_buttons = []
+        self.last_selected_tree_index = -1
+        self.last_selected_value = ""
+        self.refresh()
+        
+    def refresh(self):
+        self.last_selected_tree_index = -1
+        self.last_selected_value = ""
+        
+        try:
+            self.library_dirs = self.master.launchers[self.master.selected_launcher.get()].libraryFolders
+        except IndexError:
+            self.library_dirs = []
+        if debug: print("libdirs for " + self.master.selected_launcher.get() + " are " + str([folder.path for folder in self.library_dirs]))
+        self.destroy_widgets()
+
+        for i in range(0, len(self.library_dirs)):
+            j = (i + 1) * 2 - 1
+            self.labels.append(ttk.Label(self, text=self.library_dirs[i].path))
+            self.labels[-1].grid(column=j, row=0, sticky=("S", "W", "E"), padx=5)
+            
+            self.trees.append(ttk.Treeview(self, columns=("size", "arrow")))
+            self.trees[-1].column("size", width=100, anchor="e")
+            self.trees[-1].heading("size", text="Size")
+            self.trees[-1].column("arrow", width=100, anchor="center")
+            #self.trees[-1].bind("<<TreeviewSelect>>", self.on_selection)
+            self.trees[-1].bind("<ButtonRelease-1>", self.on_selection)
+            for game in self.library_dirs[i].games:
+                self.trees[-1].insert("", "end", game.name, text=game.name, values=(game.size, str(game.isJunction)))
+            self.trees[-1].grid(column=j, row=1, sticky=("N", "W", "E", "S"), padx=(3, 0))
+            
+            self.scrollbars.append(ttk.Scrollbar(self, orient="vertical", command=self.trees[-1].yview))
+            self.scrollbars[-1].grid(column=j+1, row=1, sticky=("N", "S"), padx=(0, 3))
+            self.trees[-1]["yscrollcommand"] = self.scrollbars[-1].set
+
+            self.buttons.append(ttk.Button(self, text="Move here", command=self.on_move_button))
+            self.buttons[-1].grid(column=j, row=2, sticky=("S", "W", "E"), padx=5)
+
+            self.columnconfigure([j], minsize=300, weight=1)#, pady=50)
+            self.columnconfigure([j+1], minsize=10, weight=0)#, pady=50)
+            self.rowconfigure([0, 2], weight=0, minsize=10)
+            self.rowconfigure([1], minsize=300, weight=1)
+
+    def destroy_widgets(self):
+        for tree in self.trees:
+            tree.destroy()
+        self.trees = []
+        for label in self.labels:
+            label.destroy()
+        self.labels = []
+        for scrollbar in self.scrollbars:
+            scrollbar.destroy()
+        self.scrollbars = []
+        for button in self.buttons:
+            button.destroy()
+        self.buttons = []
+    
+    def on_selection(self, event=None):
+            selected_items = []
+            i = 0
+            for tree in self.trees:
+                for item in tree.selection():
+                    selected_items.append((item, i))
+                tree.selection_set(())
+                i = i + 1
+            if self.last_selected_value == selected_items[0][0]:
+                self.last_selected_value = selected_items[-1][0]
+                self.last_selected_tree_index = selected_items[-1][1]
+            else:
+                self.last_selected_value = selected_items[0][0]
+                self.last_selected_tree_index = selected_items[0][1]
+
+            if debug: print("selected " + self.last_selected_value + " from tree " + str(self.last_selected_tree_index))    
+            self.trees[self.last_selected_tree_index].selection_set((self.last_selected_value, ))
+
+            for button in self.buttons:
+                button.config(state="normal")
+            self.buttons[self.last_selected_tree_index].config(state="disabled")
+
+    def on_move_button(self, event=None):
+        pass
+
+
+class LauncherDialog(tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+        self.transient(parent)
+
+        ttk.Label(self, text="Launcher name:").grid(row=0, column=0, sticky=("N", "W", "E"))
+        self.launcher_name = tk.StringVar()
+        self.entry = ttk.Entry(self, width = 20, textvariable=self.launcher_name)
+        self.entry.grid(column=0, row = 0, sticky=("N", "W", "E", "S"))
+        ttk.Button(self, text = "Done", command = self.on_button).grid(column=1, row=0, sticky=("N", "S", "W"))
+        self.entry.bind("<Return>", self.on_button)
+
+    def on_button(self, event=None):
+        self.destroy()
+
+    def show(self):
+        self.wm_deiconify()
+        #self.wait_visibility()
+        self.grab_set()
+        self.wait_window()
+        return self.launcher_name.get()
+
+class FolderDialog(tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+        self.transient(parent)
+
+        ttk.Label(self, text="Library Folder:").grid(row=0, column=0, sticky=("N", "W", "E"))
+        self.lib_folder = tk.StringVar()
+        self.entry = ttk.Entry(self, width = 40, textvariable=self.lib_folder, state="readonly")
+        self.entry.grid(column=0, row = 0, sticky=("N", "W", "E", "S"))
+        ttk.Button(self, text = "Select Folder", command = self.on_select_button).grid(column=1, row=0, sticky=("N", "S", "W"))
+        ttk.Button(self, text = "Done", command = self.on_done_button).grid(column=2, row=0, sticky=("N", "S", "W"))
+        #self.entry.bind("<Return>", self.on_done_button)
+
+    def on_done_button(self, event=None):
+        self.destroy()
+
+    def on_select_button(self, event=None):
+        self.lib_folder.set(filedialog.askdirectory())
+
+    def show(self):
+        self.wm_deiconify()
+        #self.wait_visibility()
+        self.grab_set()
+        self.wait_window()
+        return self.lib_folder.get()
+
+
+
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.title("Game Mover")
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    #MainFrame(root).grid(column=0, row=0, sticky=("N", "S", "E", "W"))
+    MainFrame(root).pack(fill="both", expand=True)
+    root.mainloop()
---
+++ src/game_mover.py
@@ -0,0 +1,97 @@
+#add launchers
+#get folder paths for launchers
+#view games for each folder
+#recognize junctions and match them
+#GUI
+
+import json
+import os
+
+
+class Launcher(object):
+    def __init__(self, name):
+        self.name = name
+        self.libraryFolders = []
+        self.index = -1
+        #match name to icon?
+    
+    def add_library_folder(self, path):
+        if not path in self.libraryFolders:
+            self.libraryFolders.append(LibraryFolder(path))
+            self.libraryFolders.sort()
+
+class LibraryFolder(object):
+    def __init__(self, path):
+        self.path = path
+        self.games = []
+        self.get_games()
+
+    def __lt__(self, other):
+        return self.path < other.path
+
+    def get_games(self):
+        gamesBak = self.games
+        try:
+            self.games = []
+            for item in os.scandir(self.path):
+                if item.is_dir():
+                    full_dir = os.path.join(self.path, item.name)
+                    if is_junction(full_dir):
+                        self.games.append(GameFolder(item.name, self.path, 0, True, os.path.realpath(full_dir)))
+                    else:
+                        self.games.append(GameFolder(item.name, self.path, self.folder_size(full_dir)))    
+        except OSError:
+            print("error getting games, wrong path?")
+            self.games = gamesBak
+
+    #https://stackoverflow.com/questions/1392413/calculating-a-directorys-size-using-python
+    def folder_size(self, path):
+        total = 0
+        for entry in os.scandir(path):
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += self.folder_size(entry.path)
+        return total
+
+
+class GameFolder(object):
+    def __init__(self, name, library, sizeInBytes = 0, isJunction = False, junctionTarget = None):
+        self.name = name
+        self.library = library
+        self.sizeInBytes = sizeInBytes
+        self.size = self.get_readable_size()
+        self.isJunction = isJunction
+        self.junctionTarget = junctionTarget
+
+    def get_readable_size(self):
+        if self.sizeInBytes > 1073741824:
+            return "{:0.2f} GB".format(self.sizeInBytes / 1024 / 1024 / 1024)
+        elif self.sizeInBytes > 1048576:
+            return "{:0.2f} MB".format(self.sizeInBytes / 1024 / 1024)
+        elif self.sizeInBytes > 1024:
+            return "{:0.2f} KB".format(self.sizeInBytes / 1024)
+        else:
+            return str(self.sizeInBytes) + " B"
+
+
+    
+
+def is_junction(path):
+    try:
+        return bool(os.readlink(path))
+    except OSError:
+        return False
+
+if __name__ == "__main__":
+    steam1 = Launcher("steam")
+    steam1.add_library_folder("S:\Steam\steamapps\common")
+    steam1.add_library_folder("T:\Steam\steamapps\common")
+    for game in steam1.libraryFolders[0].games:
+        if game.isjunction:
+            print(game.name + " --> " + game.junctionTarget)
+        else:
+            print(game.name)
+    for x in steam1.libraryFolders:
+        print(x.path)
+
