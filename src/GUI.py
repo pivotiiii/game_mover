984a6e4d82309f4de38ca5b22771b2cdeffb8303
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-01-03 19:12:03 +0100

    improved launcher dialog handling for no result position center of root add label

--- src/GUI.py
+++ src/GUI.py
@@ -166,6 +166,8 @@ class LauncherFrame(tk.Frame):
 
     def on_add_launcher(self, event=None):
         launcher = LauncherDialog(self).show()
+        if launcher == "" or launcher == None:
+            return
         self.master.launchers[launcher] = game_mover.Launcher(launcher)
         self.master.selected_launcher.set(launcher)
         self.master.libview_frame.refresh()
@@ -396,13 +398,21 @@ class LauncherDialog(tk.Toplevel):
     def __init__(self, parent):
         tk.Toplevel.__init__(self, parent)
         self.transient(parent)
-
-        ttk.Label(self, text="Launcher name:").grid(row=0, column=0, sticky=("N", "W", "E"))
+        
+        ttk.Label(self, text="Enter launcher name:").grid(row=0, column=0, sticky=("N", "W", "E"))
         self.launcher_name = tk.StringVar()
         self.entry = ttk.Entry(self, width = 20, textvariable=self.launcher_name)
-        self.entry.grid(column=0, row = 0, sticky=("N", "W", "E", "S"))
-        ttk.Button(self, text = "Done", command = self.on_button).grid(column=1, row=0, sticky=("N", "S", "W"))
+        self.entry.grid(column=0, row = 1, sticky=("W", "S"))
+        ttk.Button(self, text = "Done", command = self.on_button).grid(column=1, row=1, sticky=("S", "E"))
         self.entry.bind("<Return>", self.on_button)
+
+        rx = root.winfo_x()
+        ry = root.winfo_y()
+        rw = root.winfo_width()
+        rh = root.winfo_height()
+        x = int(rx + 0.4 * rw)
+        y = int(ry + 0.4 * rh)
+        self.geometry(f"+{x}+{y}")
 
     def on_button(self, event=None):
         self.destroy()
